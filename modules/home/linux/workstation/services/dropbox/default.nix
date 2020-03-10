18290f818c6ec8179958647586b8846a7f965c22
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:11:30 -0500

    add dropbox module

---
+++ modules/home/linux/workstation/services/dropbox/default.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  options.mine.workstation.dropbox.enable = mkEnableOption "dropbox";
+
+  config = mkIf config.mine.workstation.dropbox.enable {
+    home.packages = [ dropbox-cli ];
+    systemd.user.services.dropbox = {
+      description = "Dropbox";
+      wantedBy = [ "graphical-session.target" ];
+      environment = {
+        QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+        QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+      };
+      serviceConfig = {
+        ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+        ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+        KillMode = "control-group"; # upstream recommends process
+        Restart = "on-failure";
+        PrivateTmp = true;
+        ProtectSystem = "full";
+        Nice = 10;
+      };
+    };
+  };
+}
+
