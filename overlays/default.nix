04b14fe19049e4ba45345d2721554fd031723b27
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:15:10 -0500

    Update nixpkgs channel

--- overlays/default.nix
+++ overlays/default.nix
@@ -37,10 +37,12 @@ let
     unstable = (import (super.fetchFromGitHub {
       owner = "nixos";
       repo = "nixpkgs-channels";
-      rev = "e85c1f586807b5acd244df4c45a5130aa3f0734d";
-      sha256 = "1xy1qgam0i2fyqhaczw0qrx8yv3hgdh9jp47wmln5ljiixr5ic5n";
+      rev = "ae002fe44e96b868c62581e8066d559ca2179e01";
+      sha256 = "1bawyz3ksw2sihv6vsgbvhdm4kn63xrrj5bavg6mz5mxml9rji89";
     })) {
-      config = {};
+      config = {
+        allowUnfree = true;
+      };
       overlays = filteredModules [] ./unstable;
     };
   })];
