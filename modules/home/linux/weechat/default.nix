1d9c61a94b440b6b3ee43215cfeee80bd665ca7d
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:00:16 -0500

    Add weechat wrapper

--- modules/home/linux/weechat/default.nix
+++ modules/home/linux/weechat/default.nix
@@ -6,6 +6,19 @@ with import /nixcfg/util;
 let
   cfg = config.mine.weechat;
 
+  matrix = pkgs.weechat-matrix-bridge.overrideAttrs (old: {
+    src = pkgs.fetchFromGitHub {
+      owner = "torhve";
+      repo = "weechat-matrix-protocol-script";
+      rev = "8d32e90d864a8f3f09ecc2857cd5dd6e39a8c3f7";
+      sha256 = "0qqd6qmkrdc0r3rnl53c3yp93fbcz7d3mdw3vq5gmdqxyym4s9lj";
+    };
+  });
+
+  wrapper = pkgs.writeShellScriptBin "weechat" ''
+    ${pkgs.rxvt_unicode-with-plugins}/bin/urxvt --internalBorder 0 -e ${pkgs.weechat}/bin/weechat
+  '';
+
 in {
   options.mine.weechat.enable = mkEnableOption "weechat";
   options.mine.weechat.mutable-config = mkOption {
@@ -13,7 +26,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.weechat ];
+    home.packages = [ matrix wrapper ];
     home.activation = mkIf (cfg.mutable-config != "") {
       weechat = symlink cfg.mutable-config "~/.weechat";
     };
