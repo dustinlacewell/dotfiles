f202ecc2dd22f888d8267e138137d4bfae3b2c5a
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:08:45 -0500

    update XResources support

--- modules/home/linux/workstation/urxvt/default.nix
+++ modules/home/linux/workstation/urxvt/default.nix
@@ -5,13 +5,16 @@ with import /nixcfg/util;
 
 let
   cfg = config.mine.workstation.urxvt;
+  urxvt-bordered = pkgs.writeShellScriptBin "urxvt-bordered" ''
+    ${pkgs.rxvt_unicode-with-plugins}/bin/urxvt --internalBorder 15
+  '';
 
 in {
   options.mine.workstation.urxvt.enable = mkEnableOption "urxvt";
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.rxvt_unicode-with-plugins ];
+    home.packages = [ pkgs.rxvt_unicode-with-plugins urxvt-bordered ];
     xdg.configFile."wal/templates/Xresources".source = ./Xresources;
-    home.activation.Xresources = symlink "~/.cache/wal/Xresources" "~/.Xresources";
+    home.activation.Xresources = symlink "~/.config/wpg/formats/Xresources" "~/.Xresources";
   };
 }
