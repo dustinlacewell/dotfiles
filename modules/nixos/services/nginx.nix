2ad3922896f8fef106b1ab33213976f7649e8b8c
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:07:05 -0500

    Improve nginx syntax

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -8,10 +8,17 @@ with lib;
   config = mkIf config.mine.nginx.enable {
     services.nginx = {
       enable = true;
-      virtualHosts."localhost" = {
-        serverName = "logos.ldlework.com";
-        forceSSL = false;
-        root = "/var/www/";
+      virtualHosts = {
+        "localhost" = {
+          serverName = "logos.ldlework.com";
+          forceSSL = false;
+          root = "/var/www/";
+          # locations = {
+            #   "/" = {
+              #     proxyPass = "http://localhost:8000/";
+              #   };
+            # };
+        };
       };
     };
   };
