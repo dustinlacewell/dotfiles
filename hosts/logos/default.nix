653dd1104c901a76d6384b7feb38cef7bcb58de9
Dustin Lacewell <dlacewell@gmail.com>
2020-03-10 04:15:47 -0500

    Add syncthing

--- hosts/logos/default.nix
+++ hosts/logos/default.nix
@@ -1,5 +1,6 @@
 let
   secrets = import /nixcfg/external/private;
+  shares = import ./shares.nix;
 in {
   imports = [
     ../../modules/nixos
@@ -17,4 +18,42 @@ in {
   mine.docker.enable = true;
   mine.ddclient.enable = false;
   mine.nginx.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  users.extraUsers.ldlework.extraGroups = [ "libvirtd" "syncthing" ];
+
+  services.openssh.enable = true;
+  services.openssh.openFirewall = true;
+  nixpkgs.config.allowUnfree = true;
+  services.samba = shares;
+
+  networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [ 445 139 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
+
+  services.syncthing = {
+    enable = true;
+    user = "ldlework";
+    declarative = {
+      devices = {
+        wharf = {
+          id = "NOZ3N4H-Z7CPBFO-EBUXB6E-JWVABYI-N2HCUB4-BD7OCAL-TNYMYUW-LGC5HQR";
+        };
+        dozer = {
+          id = "SCRO7QB-ZUYDYJH-G6TXGZ3-HTZFT3M-N44VTOK-QTSETVB-2JYVABG-MOTKJQQ";
+        };
+      };
+      folders = {
+        "/home/ldlework/org" = {
+          id = "org";
+          devices = [
+            "wharf"
+            "dozer"
+          ];
+          watch = true;
+        };
+      };
+    };
+  };
+
 }
