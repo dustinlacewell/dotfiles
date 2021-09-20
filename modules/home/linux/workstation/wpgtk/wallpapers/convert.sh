72c18bee6e03516dc5c8a4863751876e7f610b05
Dustin Lacewell <dlacewell@gmail.com>
2021-09-20 09:42:24 -0500

    Remove wpgtk data

--- modules/home/linux/workstation/wpgtk/wallpapers/convert.sh
+++
@@ -1,1 +0,0 @@
-mogrify -sepia-tone 80% -modulate 100,80,100 *.{png,jpg}
--- modules/home/linux/workstation/wpgtk/wallpapers/fix.py
+++
@@ -1,7 +0,0 @@
-import os
-import pathlib
-import shutil
-
-for filename in os.listdir("."):
-    if filename.endswith("!d"):
-        shutil.move(filename, filename[:-2])
